8582774ad15f6da871c722792d7bfb33cefd2bb2
kingma-sbw <kingma@sbw-media.ch>
2022-04-01 17:10:40 +0200

    Created class files

--- classes/NeueMedien/ProjectView.php
+++ classes/NeueMedien/ProjectView.php
@@ -1,19 +1,33 @@
-<?php
+<?php declare(strict_types=1);
+
 namespace NeueMedien;
 
-class ProjectView extends \DB\DBRecord implements \DB\DBRecordInterface {
-	static public function getPrimaryKeyName() { return "ProjectID"; }
-	static public function getTableName() { return "ProjectView"; }
-	static public function getFieldNames()
-	{
-		return 
-		[
-			'ProjectNr',
-			'ProjectName',
-			'ProjectDescription',
-			'ProjectTypeName',
-      'ProjectCoach',
-      'ProjectStatus'
+/*
+ * projectview – Persistant object
+ */
+class projectview implements \DB\DBRecordInterface, \Iterator{
+	use \DB\Persist;
+
+	private $ProjectID;
+	private $ProjectNr;
+	private $ProjectName;
+	private $ProjectDescription;
+	private $ProjectTypeName;
+	private $ProjectStatus;
+	private $Coach;
+
+// Persist functions
+	static public function getPrimaryKey():string { return 'ProjectID'; }
+	static public function getTableName():string { return '`projectview`'; }
+	static public function getFields():array {
+		return [
+			'ProjectID' => ['integer', 10 ],
+			'ProjectNr' => ['string', 10 ],
+			'ProjectName' => ['string', 255 ],
+			'ProjectDescription' => ['string', 8192 ],
+			'ProjectTypeName' => ['string', 255 ],
+			'ProjectStatus' => ['integer', 3 ],
+			'Coach' => ['string', 255 ],
 		];
 	}
 }
