92ce35850da34f52ec1666aa64c8e18e04d8a80b
kingma-sbw <kingma@sbw-media.ch>
2022-04-11 11:03:02 +0200

    Add thaw/freeze signatures to IPersist

--- classes/Persist/IPersist.php
+++ classes/Persist/IPersist.php
@@ -15,7 +15,19 @@ interface IPersist
 
   public function __get(string $field);
   public function __set(string $field, $value ): void;
-  
+	/**
+	 * create – create a new record in the database or update an existing one
+	 * @return bool
+	 */
+	public function freeze( ):bool;
+  /**
+   * thaw – fetch a record from the database by key
+	 * this assumes keys are singel and ints!!
+   *
+   * @param  int $id
+   * @return object
+   */
+  public function thaw($id): ?IPersist;
   /**
    * Return the primary key of the object
    *
